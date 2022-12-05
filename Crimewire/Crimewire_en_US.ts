ae3cc8434e4a99486402066c74d694ad52f972b7
snortforce <info@codulso.xyz>
2022-12-05 13:28:34 -0600

    UI: Misc fixes

--- Crimewire/Crimewire_en_US.ts
+++ Crimewire/Crimewire_en_US.ts
@@ -22,127 +22,127 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="46"/>
+        <location filename="crimewavemain.ui" line="45"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;test&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="49"/>
+        <location filename="crimewavemain.ui" line="48"/>
         <source>test</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="59"/>
+        <location filename="crimewavemain.ui" line="58"/>
         <source>Search </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="71"/>
+        <location filename="crimewavemain.ui" line="70"/>
         <source>Search for files...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="83"/>
+        <location filename="crimewavemain.ui" line="82"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Your search goes in this box&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="96"/>
+        <location filename="crimewavemain.ui" line="95"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Click here to search!&lt;/p&gt;&lt;p&gt;&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="120"/>
+        <location filename="crimewavemain.ui" line="119"/>
         <source>Search Results</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="136"/>
-        <source>Tab 2</source>
+        <location filename="crimewavemain.ui" line="135"/>
+        <source>Results</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="161"/>
+        <location filename="crimewavemain.ui" line="166"/>
         <source>Filter by Filetype</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="181"/>
+        <location filename="crimewavemain.ui" line="186"/>
         <source>Downloading</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="196"/>
+        <location filename="crimewavemain.ui" line="201"/>
         <source>Library</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="212"/>
+        <location filename="crimewavemain.ui" line="217"/>
         <source>&amp;File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="222"/>
+        <location filename="crimewavemain.ui" line="227"/>
         <source>&amp;View</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="227"/>
+        <location filename="crimewavemain.ui" line="232"/>
         <source>&amp;Navigation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="232"/>
+        <location filename="crimewavemain.ui" line="237"/>
         <source>&amp;Resources</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="237"/>
+        <location filename="crimewavemain.ui" line="242"/>
         <source>&amp;Tools</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="242"/>
+        <location filename="crimewavemain.ui" line="247"/>
         <source>Filter&amp;s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="247"/>
+        <location filename="crimewavemain.ui" line="252"/>
         <source>&amp;Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="268"/>
+        <location filename="crimewavemain.ui" line="273"/>
         <source>Help Contents</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="271"/>
+        <location filename="crimewavemain.ui" line="276"/>
         <source>F1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="286"/>
+        <location filename="crimewavemain.ui" line="291"/>
         <source>About CrimeWire</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="291"/>
+        <location filename="crimewavemain.ui" line="296"/>
         <source>Connect</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="296"/>
+        <location filename="crimewavemain.ui" line="301"/>
         <source>Disconnect</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="301"/>
+        <location filename="crimewavemain.ui" line="306"/>
         <source>Preferences</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="crimewavemain.ui" line="306"/>
+        <location filename="crimewavemain.ui" line="311"/>
         <source>Exit CrimeWire</source>
         <translation type="unfinished"></translation>
     </message>
