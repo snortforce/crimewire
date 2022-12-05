a3cd963f1dd0622b6ef3e4568fde63a6bc77bdf3
snortforce <info@codulso.xyz>
2022-12-05 13:24:13 -0600

    SearchContext: add initial SearchContext class

---
+++ Crimewire/searchcontext.cpp
@@ -0,0 +1,6 @@
+#include "searchcontext.h"
+
+searchContext::searchContext()
+{
+
+}
---
+++ Crimewire/searchcontext.h
@@ -0,0 +1,11 @@
+#ifndef SEARCHCONTEXT_H
+#define SEARCHCONTEXT_H
+
+
+class searchContext
+{
+public:
+    searchContext();
+};
+
+#endif // SEARCHCONTEXT_H
